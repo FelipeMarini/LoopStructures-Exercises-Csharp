914c6ca0d3a736d43b61b5fa21f1b452b45c0868
Felipe Marini <felipemarini71188@gmail.com>
2020-11-19 12:40:34 -0300

    Algorithm to validate username different from password done

---
+++ Algorithm to verify username different from password/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Algorithm_to_verify_username_different_from_password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "";
+            string senha = "";
+
+            while(true) {
+                
+                Console.WriteLine("Digite seu nome de usuário:");
+                nome = Console.ReadLine();
+
+                Console.WriteLine("Digite seu senha:");
+                senha = Console.ReadLine();
+
+                if(nome == senha){
+                    Console.WriteLine("--------------------------------------------");
+                    Console.WriteLine("Erro. A senha deve ser diferente do nome de usuário, digite uma senha válida.");
+                } else {break;}
+            }
+            
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Validação feita com sucesso, seja bem vindo " + nome);
+            Console.WriteLine("--------------------------------------------");
+
+        }
+    }
+}
