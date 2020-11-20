61ddccb3f6a2bc0063189f154a335123770518c4
DEV <dev@info.local>
2020-11-20 14:42:54 -0300

    Algorithm to validate user info

--- Algorithm to verify and validate user info/Validate user info.cs
+++ Algorithm to verify and validate user info/Validate user info.cs
@@ -16,37 +16,51 @@ namespace Algorithm_to_verify_and_validate_user_info
                 Console.WriteLine("Digite seu Nome: ");
                 nome = Console.ReadLine();
                 Console.WriteLine();
+                Console.WriteLine("------------------------"); 
+                Console.WriteLine("Defina um nome válido"); }
+                
+              while(nome == "");
+                
+                Console.WriteLine();
 
+                
+            do{
                 Console.WriteLine("Digite sua Idade: ");
                 idade = int.Parse(Console.ReadLine());
                 Console.WriteLine();
+                Console.WriteLine("------------------------"); 
+                Console.WriteLine("Defina uma idade válida"); }
+                
+                while(idade <= 0 || idade > 150);
+                
+                Console.WriteLine();
 
+            
+            do{
                 Console.WriteLine("Digite seu Salário ($): ");
                 salario = float.Parse(Console.ReadLine());
                 Console.WriteLine();
+                Console.WriteLine("------------------------"); 
+                Console.WriteLine("Defina um salário válido");
+                Console.WriteLine(); }
+                
+                while(salario == 0); 
+                
+                Console.WriteLine(); 
 
+                
+            do{   
                 Console.WriteLine("Digite seu Estado Civil: s - Solteiro(a); c - Casado(a); v - Viuvo(a); d - Divorciado(a)");
                 estadocivil = (Console.ReadLine());
                 Console.WriteLine();
-
-                if(nome == "") {Console.WriteLine("------------------------"); Console.WriteLine("Defina um nome válido"); 
-                                nome = "null";}
-
-                if(idade < 0 || idade >150) {Console.WriteLine("------------------------"); Console.WriteLine("Defina uma idade válida"); 
-                                             idade = 1;}
-
-                if(salario == 0) {Console.WriteLine("------------------------"); Console.WriteLine("Defina um salário válido"); 
-                                  salario = 0;}
-
-                if(estadocivil == "s" || estadocivil == "c" || estadocivil == "v" || estadocivil == "d"){}
+                Console.WriteLine("-------------------------------"); 
+                Console.WriteLine("Defina um estado civil válido"); }
                 
-                else{
-                    Console.WriteLine("-------------------------------"); Console.WriteLine("Defina um estado civil válido");
-                    estadocivil = "null";
-                    }
-            
-            } while(nome == "null" || idade == 1 || salario == 0 || estadocivil == "null" );
-
+                while(estadocivil != "s" && estadocivil != "c" && estadocivil != "v" && estadocivil != "d");
+                
+                Console.WriteLine(); 
+                
+                
             Console.WriteLine("-------------------------------");
             Console.WriteLine("Confirme seus dados");
             Console.WriteLine("Nome: " + nome);
@@ -55,7 +69,27 @@ namespace Algorithm_to_verify_and_validate_user_info
             Console.WriteLine("Estado Civil: " + estadocivil);
             Console.WriteLine("-------------------------------");
 
-            Console.WriteLine("Validação concluida");
+            Console.WriteLine("Validação concluida"); 
         }
     }
 }
+                
+                
+                                
+
+                 
+
+                  
+                                             
+
+                  
+                                  
+
+                
+                
+                    
+                    
+                    
+            
+            
+
